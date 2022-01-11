23c6c102bdda992e8aac21376167b47bde63eac7
Anicka <anau234@gmail.com>
2022-01-11 17:19:38 +0100

    Add basic functionality, winning conditions

--- tictactoe.py
+++ tictactoe.py
@@ -1,1 +1,43 @@
-print("Hello World!")
+from wincheck import check_lines, check_diagonals
+
+gamegrid = [
+[["X"], ["_"], ["_"], ["_"], ["_"], ["_"], ["X"], ["_"], ["_"], ["_"]],
+[["_"], ["X"], ["_"], ["_"], ["_"], ["_"], ["X"], ["_"], ["_"], ["_"]],
+[["_"], ["_"], ["X"], ["_"], ["_"], ["X"], ["O"], ["X"], ["_"], ["_"]],
+[["_"], ["_"], ["_"], ["X"], ["_"], ["_"], ["X"], ["O"], ["_"], ["_"]],
+[["_"], ["_"], ["_"], ["X"], ["X"], ["X"], ["X"], ["_"], ["_"], ["_"]],
+[["_"], ["_"], ["_"], ["_"], ["_"], ["O"], ["O"], ["_"], ["X"], ["_"]],
+[["_"], ["_"], ["_"], ["_"], ["X"], ["X"], ["X"], ["X"], ["_"], ["X"]],
+[["_"], ["_"], ["_"], ["O"], ["_"], ["_"], ["X"], ["_"], ["_"], ["_"]],
+[["_"], ["_"], ["_"], ["_"], ["_"], ["_"], ["X"], ["_"], ["_"], ["_"]],
+[["_"], ["_"], ["_"], ["_"], ["_"], ["O"], ["X"], ["O"], ["O"], ["O"]]
+]
+
+size = 10
+
+def loadgrid(grid):
+    for i in range(size):
+        for j in range(size):
+            if j == size - 1:
+                print(grid[i][j])
+            else:
+                print(grid[i][j], end="")
+
+
+def check_win(grid):
+    for i in range(size):
+        for j in range(size):
+            if grid[i][j] == ["X"]:
+                if check_lines(i, j, "X", grid, size) == True:
+                    return "X has won"
+                if check_diagonals(i, j, "X", grid, size) == True:
+                    return "X has won"
+            if grid[i][j] == ["O"]:
+                if check_lines(i, j, "O", grid, size) == True:
+                    return "O has won"
+                if check_diagonals(i, j, "O", grid, size) == True:
+                    return "O has won"
+    return "No one has won"
+
+loadgrid(gamegrid)
+print(check_win(gamegrid))
---
+++ wincheck.py
@@ -0,0 +1,99 @@
+def check_down(i, j, symbol, grid):
+    total = 0
+    for x in range(5):
+        if grid[i + x][j] == [symbol]:
+            total += 1
+    if total == 5:
+        return True
+
+
+def check_up(i, j, symbol, grid):
+    total = 0
+    for x in range(5):
+        if grid[i - x][j] == [symbol]:
+            total += 1
+    if total == 5:
+        return True
+
+
+def check_left(i, j, symbol, grid):
+    total = 0
+    for x in range(5):
+        if grid[i][j - x] == [symbol]:
+            total += 1
+    if total == 5:
+        return True
+
+
+def check_right(i, j, symbol, grid):
+    total = 0
+    for x in range(5):
+        if grid[i][j + x] == [symbol]:
+            total += 1
+    if total == 5:
+        return True
+
+
+def check_left_up(i, j, symbol, grid):
+    total = 0
+    for x in range(5):
+        if grid[i - x][j - x] == [symbol]:
+            total += 1
+    if total == 5:
+        return True
+
+
+def check_left_down(i, j, symbol, grid):
+    total = 0
+    for x in range(5):
+        if grid[i + x][j - x] == [symbol]:
+            total += 1
+    if total == 5:
+        return True
+
+
+def check_right_up(i, j, symbol, grid):
+    total = 0
+    for x in range(5):
+        if grid[i - x][j + x] == [symbol]:
+            total += 1
+    if total == 5:
+        return True
+
+def check_right_down(i, j, symbol, grid):
+    total = 0
+    for x in range(5):
+        if grid[i + x][j + x] == [symbol]:
+            total += 1
+    if total == 5:
+        return True
+
+
+def check_lines(i, j, symbol, grid, size):
+    if i < size - 4:
+        if check_down(i, j, symbol, grid) == True:
+            return True
+    if i > size - 7:
+        if check_up(i, j, symbol, grid) == True:
+            return True
+    if j < size - 4:
+        if check_right(i, j, symbol, grid) == True:
+            return True
+    if j > size - 7:
+        if check_left(i, j, symbol, grid) == True:
+            return True
+
+
+def check_diagonals(i, j, symbol, grid, size):
+    if i > size - 7 and j > size - 7:
+        if check_left_up(i, j, symbol, grid) == True:
+            return True
+    if i < size - 4 and j > size - 7:
+        if check_left_down(i, j, symbol, grid) == True:
+            return True
+    if i > size - 7 and j < size - 4:
+        if check_right_up(i, j, symbol, grid) == True:
+            return True
+    if i < size - 4 and j < size - 4:
+        if check_right_down(i, j, symbol, grid) == True:
+            return True
