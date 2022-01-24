df9656b2479eebd4a8caf1dcb313e59e850c17a3
Anicka <anau234@gmail.com>
2022-01-24 17:43:15 +0100

    Remove mistakes

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import random
+from xml.etree.ElementPath import find
 
 
 gamegrid = [
@@ -8,7 +9,7 @@ gamegrid = [
 [["_"], ["_"], ["_"], ["_"], ["_"], ["_"], ["_"], ["_"], ["_"], ["_"]],
 [["_"], ["_"], ["_"], ["_"], ["_"], ["_"], ["_"], ["_"], ["_"], ["_"]],
 [["_"], ["_"], ["_"], ["_"], ["_"], ["_"], ["_"], ["_"], ["_"], ["_"]],
-[["_"], ["_"], ["_"], ["_"], ["_"], ["X"], ["_"], ["_"], ["_"], ["_"]],
+[["_"], ["_"], ["_"], ["_"], ["_"], ["_"], ["_"], ["_"], ["_"], ["_"]],
 [["_"], ["_"], ["_"], ["_"], ["_"], ["_"], ["_"], ["_"], ["_"], ["_"]],
 [["_"], ["_"], ["_"], ["_"], ["_"], ["_"], ["_"], ["_"], ["_"], ["_"]],
 [["_"], ["_"], ["_"], ["_"], ["_"], ["_"], ["_"], ["_"], ["_"], ["_"]]
@@ -25,11 +26,11 @@ def loadgrid(grid):
                 print(grid[i][j], end="")
 
 def checkSpaces(i, j, size, grid):
-    if i >= 0 and j >= 0 and i < size and j < size and grid[i][j] != ["X"]:
+    if i >= 0 and j >= 0 and i < size and j < size and grid[i][j] == ["_"]:
         return True
     return False
 
-def findPrimaryValue(grid):
+def findPrimaryValue(grid, size):
     possible_moves = []
     best_value = float("-inf")
     for i in range(size):
@@ -55,19 +56,16 @@ def findPrimaryValue(grid):
                 
                 if value > best_value:
                     best_value = value
-                
-                possible_moves.append([i, j, value])
-                
+                possible_moves.append([i, j, value])   
+ 
     return best_value, possible_moves
 
 
-def getInitialMove(best_value, possible_moves):
+def getMove(best_value, possible_moves):
     best_moves = []
-    for i in range(pow(size, 2) - 1):
+    x = len(possible_moves)
+    for i in range(x - 1):
         if possible_moves[i][2] == best_value:
             best_moves.append(possible_moves[i])
 
     return random.choice(best_moves)
-
-x, y = findPrimaryValue(gamegrid)
-print(getInitialMove(x, y))
