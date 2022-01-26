033e760c1126465945a14fb16c4a1d65d2ad8cf5
Anicka <anau234@gmail.com>
2022-01-26 21:48:35 +0100

    Begin best move decision making process

--- bot.py
+++ bot.py
@@ -1,15 +1,15 @@
 import random
 
-# def create_grid(size):
-#     grid = []
-#     for i in range(size):
-#         row = []
-#         for j in range(size):
-#             row.append(["_"])
+def create_grid(size):
+    grid = []
+    for i in range(size):
+        row = []
+        for j in range(size):
+            row.append(["_"])
         
-#         grid.append(row)
+        grid.append(row)
 
-#     return grid, size
+    return grid, size
 
 # def load_grid(grid, size):
 #     for i in range(size):
@@ -32,9 +32,25 @@ def check_spaces(i, j, size, grid, player_symbol):
         return True
     return False
 
-def check_potential_development(i, j, size, grid, computer_symbol):
-    if i >= 0:
-        pass
+def check_if_empty(i, j, size, grid):
+    if i >= 0 and j >= 0 and i < size and j < size and grid[i][j] == ["_"]:
+        return True
+    return False
+
+
+def check_potential_development(i, j, grid, player_symbol, y, x):
+    # y -> i, x -> j
+    value = 0
+    for num in range(1, 5):
+        space = grid[i + num * y][j + num * x]
+        if space == ["_"]:
+            value += 1
+        elif space == [player_symbol]:
+            value += 10
+    
+    move = [i, j, value]
+    return move
+
 
 def find_primary_value(grid, size):
     possible_moves = []
@@ -70,13 +86,22 @@ def find_primary_value(grid, size):
 
 
 def find_move_value(grid, size):
+    possible_moves = []
     for i in range(size):
         for j in range(size):
             if grid[i][j] == ["_"]:
-
+                pass
+                # Checks if right side is suitable for playing
+                if check_if_empty(i, j + 1, size, grid) and check_spaces(i, j + 4, size, grid, "X"):
+                    possible_moves.append(check_potential_development(i, j, grid, "X", 0, 1))
+                
                 # Checks if left side is suitable for playing
-                if check_spaces(i, j + 1, size, grid, "_"):
-                    pass
+                if check_if_empty(i, j - 1, size, grid) and check_spaces(i, j - 4, size, grid, "X"):
+                    possible_moves.append(check_potential_development(i, j, grid, "X", 0, -1))
+                
+                
+                
+    return possible_moves
 
 
 
@@ -89,7 +114,11 @@ def get_move(best_value, possible_moves):
 
     return random.choice(best_moves)
 
-# gamegrid, size = create_grid(5)
+gamegrid, size = create_grid(10)
 # place_symbol(gamegrid, "X", [2, 3, 3])
 # x, y, grid = find_primary_value(gamegrid, size)
 # load_grid(grid, size)
+i, j = 6, 7
+print(find_move_value(gamegrid, size))
+print(check_spaces(i, j + 1, size, gamegrid, "X"))
+print(check_if_empty(i, j + 4, size, gamegrid))
